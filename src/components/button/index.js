dd2f869adff5a4861fdfcc677488b267f7d9aae9
Swizzy11 <122433638+Swizzy11@users.noreply.github.com>
2023-01-16 15:18:56 +0300

    Delete src directory

--- src/components/button/index.js
+++
@@ -1,6 +0,0 @@
-import Handlebars from 'handlebars';
-import tpl from 'bundle-text:./index.hbs';
-
-export default (id, value) => {
-    return Handlebars.compile(tpl)({id, value});
-}
--- src/pages/home/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from "handlebars";
-import tpl from 'bundle-text:./index.hbs';
-
-
-
-
-export default (props = {}) => {
-    return Handlebars.compile(tpl)(props);
-}
